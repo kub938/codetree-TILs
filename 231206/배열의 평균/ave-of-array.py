e9748304497b97998483ae770dfa6f668da6c4f1
yunbae <kub938@naver.com>
2023-12-06 15:22:23 +0900

    Title: 배열의 평균, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 231206/배열의 평균/ave-of-array.py
+++ 231206/배열의 평균/ave-of-array.py
@@ -1,30 +1,19 @@
-arr = [list(map(int,input().split()))for _ in range(2)]
+arr = [list(map(int,input().split())) for _ in range(2)]
+total_sum = []
+avg1=0
+avg2=0
+for i in range(2):
+    avg1 = sum(arr[i])/4
+    total_sum.append(avg1)
+    print(avg1,end=" ")
+print()
 
-def width(arr):
-    width_avg = []
-    for i in arr:
-        width_avg.append(sum(i)/(len(i)))
-    return width_avg
-
-def height(arr):
-    height_avg = []
-    h_sum=0
-    for i in range(4):
-        for j in range(2):
-            h_sum+=arr[j][i]
-        height_avg.append(h_sum/2)
-        h_sum=0
-    return height_avg
-
-def all_avg(arr):
-    a_avg = 0
-    for i in arr:
-        a_avg+=sum(i)
-    return a_avg/8
-        
-
-    
-
-print(*width(arr))
-print(*height(arr))
-print(all_avg(arr))
+for j in range(4):
+    for k in range(2):
+        avg2+=arr[k][j]
+    avg2/=2
+    total_sum.append(avg2)
+    print(avg2,end=" ")
+    avg2=0
+print()
+print(f'{sum(total_sum)/6:.1f}')
