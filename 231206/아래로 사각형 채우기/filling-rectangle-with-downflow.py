17fd409161756c154a86016bb0547f6a43904968
yunbae <kub938@naver.com>
2023-12-06 16:31:59 +0900

    Title: 아래로 사각형 채우기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 231206/아래로 사각형 채우기/filling-rectangle-with-downflow.py
+++ 231206/아래로 사각형 채우기/filling-rectangle-with-downflow.py
@@ -1,19 +1,12 @@
 n = int(input())
-
 arr = [[0 for _ in range(n)]for _ in range(n)]
-cnt=1
-col = 0
-row = 0
-
-while 1:
-    if row < n:
-        arr[row][col] = cnt
+cnt = 1
+for i in range(n):
+    for j in range(n):
+        arr[j][i] = cnt 
         cnt+=1
-        row+=1
-    elif row == n:
-        col+=1 
-        row = 0
-    if cnt>n*n:
-        for i in arr:
-            print(*i)
-        break
+
+for k in range(n):
+    for t in range(n):
+        print(arr[k][t],end=" ")
+    print()
