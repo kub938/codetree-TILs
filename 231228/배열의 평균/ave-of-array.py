da785f180525b579b71ae518d2d1b5dd96f503d3
yunbae <kub938@naver.com>
2023-12-28 18:02:26 +0900

    Title: 배열의 평균, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231228/배열의 평균/ave-of-array.py
@@ -0,0 +1,22 @@
+arr = [list(map(int,input().split()))for _ in range(2)]
+n = len(arr[0])
+
+
+row_sum1 = sum(arr[0])
+row_sum2 = sum(arr[1])
+
+avg_sum1 = row_sum1/4
+avg_sum2 = row_sum2/4
+
+print(avg_sum1, avg_sum2)
+
+
+for row in range(n):
+    sum_col = 0
+    for col in range(2):
+        sum_col += arr[col][row]
+    avg_col = sum_col/2
+    print(avg_col,end=" ")
+print()
+
+print((row_sum1+row_sum2)/8)
