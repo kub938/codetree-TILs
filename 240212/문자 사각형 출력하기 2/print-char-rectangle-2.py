219ae693c3c0e6498240018d27817acd03585728
yunbae <kub938@naver.com>
2024-02-12 17:57:57 +0900

    Title: 문자 사각형 출력하기 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240212/문자 사각형 출력하기 2/print-char-rectangle-2.py
@@ -0,0 +1,38 @@
+n = int(input())
+result = [[0]*n for _ in range(n)]
+
+def change_eng(cnt):
+    if cnt>90:
+        cnt-=36
+        return chr(cnt)
+    else:
+        return chr(cnt)
+
+#A = 65
+# 65~ 쭉 출력
+# for col 1,n+1:
+#  
+#    if col%2==0 
+    #     for row 1,n+1:
+    #             #행-1
+    # else: 
+    #     for :
+    #         행+1
+#  열+1
+cnt = 65
+
+for col in range(n):
+    if col % 2 != 0:
+        for row in range(n - 1, -1, -1):
+            eng = change_eng(cnt)
+            result[row][col] = eng
+            cnt += 1
+    else:
+        for row in range(n):
+            eng = change_eng(cnt)
+            result[row][col] = eng
+            cnt += 1
+    
+
+for i in result:
+    print(*i)
