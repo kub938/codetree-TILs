419a2d40ee7880ac6af5a4fcabbe7bbb3ccc167d
yunbae <kub938@naver.com>
2024-02-25 18:25:46 +0900

    Title: 되돌아오기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240225/되돌아오기/come-back.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+dir = {'E': 0,'W':1,'S':2,'N':3}
+dx,dy = [0,0,1,-1],[1,-1,0,0]
+nx,ny = 0,0
+x,y = 0,0
+cnt=0
+end =0
+for i in range(n):
+    d,t = input().split()
+    t = int(t)
+    for j in range(t):
+        nx,ny = dx[dir[d]],dy[dir[d]]
+        x,y = x+nx,y+ny
+        cnt+=1
+        if x==0 and y==0:
+            print(cnt)
+            break
+    end+=t
+if cnt==end:
+    print(-1)
