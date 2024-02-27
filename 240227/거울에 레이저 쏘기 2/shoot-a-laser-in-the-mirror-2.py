3b0a9101a07195a2e4d2dea3136ba36253df0d1e
yunbae <kub938@naver.com>
2024-02-27 17:40:52 +0900

    Title: 거울에 레이저 쏘기 2, Time: 151ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240227/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240227/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -22,23 +22,22 @@ elif k // n == 2:
         x, y = n-1, n - 1
         now_dir = 'L'
     else:
-        x, y = n - 1, k % n - 1
+        x, y = n - 1, -(k % n) + 3
         now_dir = 'U'
 elif k // n == 3:
     if k%n==0:
         x, y = n - 1, 0
         now_dir = 'U'
     else:
-        x, y =  n-1, 0
+        x, y = -(k % n) + 3, 0
         now_dir = 'R'
 elif k// n ==4:
     x,y = 0,0
     now_dir ='R'
 
-mirx1, miry1 = [-1, 1, 0, 0], [0, 0, -1, 1]  # \\\\
-mirx2, miry2 = [1, -1, 0, 0], [0, 0, -1, 1]  # ////
-change_dir1 = {'U': 'L', 'D': 'R', 'L': 'U', 'R': 'D'}
-change_dir2 = {'U': 'R', 'D': 'L', 'L': 'U', 'R': 'D'}
+mirx, miry = [-1, 1, 0, 0], [0, 0, -1, 1]  # \\\\
+change_dir1 = {'U': 'L', 'D': 'R', 'L': 'U', 'R': 'D'}# \\\\
+change_dir2 = {'U': 'R', 'D': 'L', 'L': 'D', 'R': 'U'}# ////
 dir_num = {'U': 0, 'D': 1, 'L': 2, 'R': 3}
 
 
@@ -51,13 +50,14 @@ while 1:
     if arr[x][0][y] == '\\':
         now_dir = change_dir1[now_dir]  # 다시 반사
         dir = dir_num[now_dir]
-        dx, dy = mirx1[dir], miry1[dir]  # 방향에 따른 x,y값 설정
     elif arr[x][0][y] == '/':
         now_dir = change_dir2[now_dir]
         dir = dir_num[now_dir]
-        dx, dy = mirx2[dir], miry2[dir]  # 방향에 따른 x,y값 설정
-    x, y = x + dx, y + dy  # 반사된 방향으로 전진
     cnt += 1
+    if not in_range(x, y):
+        break
+    dx, dy = mirx[dir], miry[dir]  # 방향에 따른 x,y값 설정
+    x, y = x + dx, y + dy  # 반사된 방향으로 전진
 
 
 
