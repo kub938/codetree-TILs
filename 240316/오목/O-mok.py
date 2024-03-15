5ef1d22d25ba0b846faf08438f2f74f4586e8ce6
yunbae <kub938@naver.com>
2024-03-16 02:23:25 +0900

    Title: 오목, Time: 148ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240316/오목/O-mok.py
+++ 240316/오목/O-mok.py
@@ -5,7 +5,7 @@ num = 0
 winner = 0
 winpos = []
 for i in range(19):
-    for j in range(19):
+    for j in range(14):
         if board[i][j]!=0:
             num = board[i][j]
             for k in range(5):
@@ -19,6 +19,8 @@ for i in range(19):
                     winpos.append(j+k-1)
                     break
             cnt=0
+for i in range(14):
+    for j in range(19):
             for k in range(5):
                 if num==board[i+k][j]:
                     cnt+=1 
@@ -30,6 +32,8 @@ for i in range(19):
                     winpos.append(j+1)
                     break
             cnt=0
+for i in range(14):
+    for j in range(14):
             for k in range(5):
                 if num==board[i+k][j+k]:
                     cnt+=1 
@@ -41,6 +45,8 @@ for i in range(19):
                     winpos.append(j+k-1)
                     break
             cnt=0
+for i in range(14):
+    for j in range(14):
             for k in range(5):
                 if j>4 and num==board[i+k][j-k]:
                     cnt+=1 
