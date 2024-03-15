2751a081026b5fc78fa0c93007835c9824dc92b2
yunbae <kub938@naver.com>
2024-03-15 17:45:28 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 143ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240315/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,10 @@
+s = input()
+n = len(s)
+cnt = 0
+for i in range(n-1):
+    if s[i]=='(' and s[i+1]=='(':
+        for j in range(i+2,n-1):
+           if s[j]==')' and s[j+1]==')':
+                cnt+=1
+
+print(cnt)
