452b75d697f91ad4d5df18ccc43c46426c8ea14a
yunbae <kub938@naver.com>
2024-03-16 02:11:07 +0900

    Title: 오목, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240316/오목/O-mok.py
@@ -0,0 +1,45 @@
+board = [list(map(int,input().split())) for _ in range(19)]
+
+cnt = 0
+num = 0
+winner = 0
+winpos = []
+for i in range(19):
+    for j in range(19):
+        if board[i][j]!=0:
+            num = board[i][j]
+            for k in range(5):
+                if num==board[i][j+k]:
+                    cnt+=1
+                else:
+                    cnt=0 
+                if cnt==5:
+                    winner=num
+                    winpos.append(i+1)
+                    winpos.append(j+k-1)
+                    break
+            cnt=0
+            for k in range(5):
+                if num==board[i+k][j]:
+                    cnt+=1 
+                else:
+                    cnt=0 
+                if cnt==5:
+                    winner=num
+                    winpos.append(i+k-1)
+                    winpos.append(j+1)
+                    break
+            cnt=0
+            for k in range(5):
+                if num==board[i+k][j+k]:
+                    cnt+=1 
+                else:
+                    cnt=0 
+                if cnt==5:
+                    winner=num
+                    winpos.append(i+k-1)
+                    winpos.append(j+k-1)
+                    break
+
+print(winner)
+print(*winpos)
