5ad8c1a2a5cd9664abbbd16618e20de3efae46ea
yunbae <kub938@naver.com>
2024-03-17 21:59:11 +0900

    Title: G or H 3, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240317/G or H 3/G-or-H-3.py
@@ -0,0 +1,30 @@
+import sys
+
+n,k =map(int,input().split())
+n_arr = []
+alp_arr = []
+num,alp = 0,''
+score = {'G':1,'H':2,0:0}
+sum_score = 0
+max_score = -sys.maxsize
+
+for i in range(n):
+    num, alp = input().split()
+    n_arr.append(num)
+    alp_arr.append(alp)
+
+n_arr = list(map(int,n_arr))
+n = max(n_arr)
+
+arr = [0 for _ in range(n+1)]
+
+for i,a in zip(n_arr,alp_arr):
+    arr[i] = a
+
+for i in range(n-k+1):
+    sum_score = 0
+    for j in range(i,i+k+1):
+        sum_score+=score[arr[j]]
+    max_score = max(max_score,sum_score)
+
+print(max_score)
