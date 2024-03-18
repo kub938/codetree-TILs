7ac9a834696680fcb31ef70a71d70a9fad7fc5c8
yunbae <kub938@naver.com>
2024-03-18 18:05:20 +0900

    Title: 아름다운 수열 2, Time: 156ms, Memory: 24MB, Status: Passed - CodeTree

--- 240318/아름다운 수열 2/beautiful-sequence-2.py
+++ 240318/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,19 +1,11 @@
 n,m = map(int,input().split())
 a = input().split()
 b = input().split()
-a = "".join(a)
-bnum_arr = set()
-cnt = 0 
 
+cnt = 0
 
-for i in range(m):
-    for j in range(m):
-        for k in range(m):
-            if i!=j and j!=k and i!=k:
-                bnum_arr.add(b[i]+b[j]+b[k])
-
-for elem in bnum_arr:
-    if elem in a:
+for i in range(n-m+1):
+    if sorted(a[i:i+m]) == sorted(b):
         cnt+=1
 
 print(cnt)
