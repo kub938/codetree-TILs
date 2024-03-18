f52a7a30b119247151fe1059c61134ca4272b13e
yunbae <kub938@naver.com>
2024-03-18 18:36:38 +0900

    Title: 바구니 안의 사탕 2, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240318/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240318/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,5 +1,5 @@
 n,k = tuple(map(int,input().split()))
-arr = [0] * 100
+arr = [0] * 101
 k = 2*k+1
 max_sum = 0
 
@@ -7,7 +7,10 @@ for _ in range(n):
     c,p = list(map(int,input().split()))
     arr[p]+= c
 
-for i in range(len(arr)-k+1):
-    max_sum = max(sum(arr[i:k+i]),max_sum)
+if k>len(arr):
+    max_sum = sum(arr)
+else:
+    for i in range(len(arr)-k+1):
+        max_sum = max(sum(arr[i:k+i]),max_sum)
 
 print(max_sum)
