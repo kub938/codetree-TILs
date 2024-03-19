db4bd283dda05599ae09011bed5df89ab7ec2f15
yunbae <kub938@naver.com>
2024-03-19 17:52:59 +0900

    Title: 밭의 높이를 고르게하기, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240319/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240319/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -1,12 +1,18 @@
+import sys
+
 n,h,t = tuple(map(int,input().split()))
 arr = list(map(int,input().split()))
-tmp = []
-sum_h = 0
+h_diff = []
+
+min_value = sys.maxsize
 
 for i in range(n):
-    tmp.append(abs(arr[i]-h))
+    h_diff.append(abs(arr[i]-h))
 
-tmp = sorted(tmp)
-sum_h = sum(tmp[:t])
+for i in range(n-t+1):
+    sum_h = 0
+    for j in range(i,i+t):
+        sum_h += h_diff[j]
+    min_value = min(sum_h,min_value)
 
-print(sum_h)
+print(min_value)
