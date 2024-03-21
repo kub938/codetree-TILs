686cc45cf3f1a972feb8e598bbc29403ef705bb3
yunbae <kub938@naver.com>
2024-03-21 21:52:44 +0900

    Title: 개발팀의 능력, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240321/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240321/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -1,6 +1,5 @@
 arr = list(map(int,input().split()))
 n = len(arr)
-check = True
 diff = 5000
 
 for i in range(n):
@@ -10,8 +9,6 @@ for i in range(n):
                 if k==i or k==j or l==i or l==j:
                     continue
                 else:
-                    check = False
-
                     sum1 = 0
                     sum2 = 0
                     sum3 = 0
@@ -20,12 +17,10 @@ for i in range(n):
                     sum2 = arr[k]+arr[l]
                     sum3 = sum(arr)-(sum1+sum2)
                     if sum1==sum2 or sum2==sum3 or sum1==sum3:
-                        check = True
                         continue
-
                     diff = min(diff, max(sum1,sum2,sum3)-min(sum1,sum2,sum3))
 
-if check:
+if diff==5000 :
     print(-1)
 else:
     print(diff)
