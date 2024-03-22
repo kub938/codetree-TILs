f0d877b5a6a6dcbcab8a133203c1db297a4d7b6a
yunbae <kub938@naver.com>
2024-03-22 17:59:06 +0900

    Title: 운행 되고 있는 시간, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240322/운행 되고 있는 시간/hours-in-service.py
+++ 240322/운행 되고 있는 시간/hours-in-service.py
@@ -1,10 +1,28 @@
 n = int(input())
-arr = []
+arr = [list(map(int,input().split())) for _ in range(n)]
+max_time = 0
 
 for i in range(n):
-    a,b = map(int,input().split())
-    arr.append(b-a)
+    time = [0]*1001
+    for j in range(i,n):
+        if i==j:
+            continue
+        else:
+            cnt=0
+            for a,b in enumerate(arr[j]):
+                for k in range(a,b):
+                    time[k]+=1
+            for elem in time:
+                if elem>0:
+                    cnt+=1
+            max_time=max(max_time,cnt)
+            
 
-ans = sum(arr)-max(arr)
 
-print(ans)
+            
+
+    
+
+
+
+print(max_time)
