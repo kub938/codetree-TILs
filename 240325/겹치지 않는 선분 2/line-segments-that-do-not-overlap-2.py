ff998112c85c882d6fc17eca77829e0ecf532f42
yunbae <kub938@naver.com>
2024-03-25 16:43:39 +0900

    Title: 겹치지 않는 선분 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240325/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240325/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -7,10 +7,9 @@ for i in range(len(x_value)):
     for j in range(i+1,len(x_value)):
         x1,x2 = x_value[i]
         x3,x4 = x_value[j]
-        if x1<x3<x2 and x1<x4<x1 or x3<x1<x4 and x3<x2<x4:
+        if x1<x3<x2 and x1<x4<x2 or x3<x1<x4 and x3<x2<x4:
             del x_value[j]
             del x_value[i]
-            break
 
 print(len(x_value))
 
