4e5553aad234f0aaf929f10f54789a86a36274f9
yunbae <kub938@naver.com>
2024-03-25 16:55:09 +0900

    Title: 겹치지 않는 선분 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240325/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240325/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,17 +1,25 @@
 n= int(input())
 x_value = [tuple(map(int,input().split())) for _ in range(n)]
 
-cnt = 0
+ans = 0
 
-for i in range(len(x_value)):
-    for j in range(i+1,len(x_value)):
+for i in range(n):
+    overlap = False
+    for j in range(n):
+        if j == i :
+            continue
+
         x1,x2 = x_value[i]
         x3,x4 = x_value[j]
-        if x1<x3<x2 and x1<x4<x2 or x3<x1<x4 and x3<x2<x4:
-            del x_value[j]
-            del x_value[i]
+        
+        if x1<=x3<=x2 and x1<=x4<=x2 or x3<=x1<=x4 and x3<=x2<=x4:
+            overlap = True
+            break
+            
+    if overlap == False:
+        ans +=1
 
-print(len(x_value))
+print(ans)
 
 
         # if arr[x:x2] in 0:
