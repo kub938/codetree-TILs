b5c085f3e8ee3a399bcae942551290a85c9f178c
yunbae <kub938@naver.com>
2024-03-26 17:02:02 +0900

    Title: 개발자의 순위, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240326/개발자의 순위/developer's-rank.py
+++ 240326/개발자의 순위/developer's-rank.py
@@ -1,16 +1,18 @@
 k,n = map(int,input().split())
-cnt = [[1]*n]*n
+cnt = [[1]*n for _ in range(n)]
 
-for i in range(k):
+for k in range(k):
     arr = list(map(int,input().split()))
     arr.reverse()
-    for j in range(i+1,n):
-        main = arr[i]-1
-        elem = arr[j]-1
-        cnt[main][elem]=0
+    for i in range(n):
+        for j in range(i+1,n):
+            main = arr[i]-1
+            elem = arr[j]-1
+            cnt[main][elem]=0
 
 ans = 0
+print(cnt)
 for i in range(n):
     ans += cnt[i].count(1)
 
-print(ans)
+print(ans-n)
