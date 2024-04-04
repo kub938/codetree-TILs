a8e6a3758994ae2e3eee7a48498149ae0d8790b4
yunbae <kub938@naver.com>
2024-04-04 17:33:30 +0900

    Title: 기수 정렬 구현, Time: 327ms, Memory: 40MB, Status: Passed - CodeTree

--- 240404/기수 정렬 구현/implement-radix-sort.py
+++ 240404/기수 정렬 구현/implement-radix-sort.py
@@ -1,26 +1,38 @@
 def radix_sort(n,arr):
-    new_arr = [[] for _ in range(10)]
-    for i in range(len(str(arr[0]))):
+    for i in range(len(arr[0])):
+        new_arr = [[] for _ in range(10)]
+
         for j in range(n):
             s = str(arr[j])
-            new_arr[int(s[-i-1])].append(arr[j])
+            new_arr[int(s[-i-1])].append(arr[j])           
         
-        for j in range(10):
-            store_arr=[]
-            for k in new_arr:
-                if k==[]:
-                    continue
-                else:
-                    store_arr.append(*k)
+        
+          
+        store_arr=[] 
+        for k in new_arr:
+            if k==[]:
+                continue
+            else:
+                for l in k:
+                    store_arr.append(l)
         arr=store_arr
-    
     return arr
 
 n = int(input())
-arr = list(map(int,input().split()))
+arr = list(input().split())
 
+max_len = 0 
+for i in arr:
+    max_len = max(max_len,len(i))
+
+for i in range(len(arr)):
+    arr[i] = arr[i].zfill(max_len)
 
 ans = radix_sort(n,arr)
 
 for elem in ans:
+    elem = elem.lstrip('0')
     print(elem,end=" ")
+
+
+# 가장큰값 찾아서 길이 잰다음 zfill로 다른애들도 0 으로 채워넣어
