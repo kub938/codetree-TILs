b057cf50168f66f994e6afd8c1c1b1928e47261b
yunbae <kub938@naver.com>
2024-04-12 18:46:11 +0900

    Title: 정수 명령 처리, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240412/정수 명령 처리/process-numeric-commands.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = [list(input().split()) for _ in range(n)]
+stack = []
+for i in range(n):
+ 
+    if 'push' in arr[i]:
+        stack.append(int(arr[i][1]))
+    elif 'size' in arr[i]:
+        print(len(stack))
+    elif 'empty' in arr[i]:
+        if len(stack)>0:
+            print(0)
+        else:
+            print(1)
+    elif 'pop' in arr[i]:
+        print(stack.pop())
