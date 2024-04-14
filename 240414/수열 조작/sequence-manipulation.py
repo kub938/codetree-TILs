d37fbdd967c2dc7948c9eec3d905dbaa00559e1a
yunbae <kub938@naver.com>
2024-04-14 17:57:30 +0900

    Title: 수열 조작, Time: 237ms, Memory: 41MB, Status: Passed - CodeTree

---
+++ 240414/수열 조작/sequence-manipulation.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+
+n = int(input())
+dq = deque([i for i in range(1,n+1)])
+
+while len(dq)>1:
+    dq.popleft()
+    tmp = dq.popleft()
+    dq.append(tmp)
+
+print(dq[0])
