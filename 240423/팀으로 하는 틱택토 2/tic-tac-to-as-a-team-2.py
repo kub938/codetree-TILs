6ca05168954ace782a95cb063c21f933fd301068
yunbae <kub938@naver.com>
2024-04-23 22:20:05 +0900

    Title: 팀으로 하는 틱택토 2, Time: 244ms, Memory: 24MB, Status: Passed - CodeTree

--- 240423/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240423/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -3,6 +3,7 @@ arr = [list(input()) for _ in range(3)]
 
 cnt=0
 cross1,cross2 = [],[]
+ans = []
 for i in range(3):
     col = []
     for j in range(3):
@@ -11,13 +12,14 @@ for i in range(3):
             cross1.append(arr[i][j])
             cross2.append(arr[-i-1][j])
     if arr[i].count(arr[i][0])==2 or arr[i].count(arr[i][1])==2 :
-        cnt+=1
+        ans.append(''.join(arr[i]))
     if col.count(col[0])==2 or col.count(col[1])==2:
-        cnt+=1
+        ans.append(''.join(col))
+        
   
 if cross1.count(cross1[0])==2 or cross1.count(cross1[1])==2:
-    cnt+=1
+    ans.append(''.join(cross1))
 if cross2.count(cross2[0])==2 or cross2.count(cross2[1])==2:
-    cnt+=1
+    ans.append(''.join(cross2))
 
-print(cnt)
+print(len(set(ans)))
