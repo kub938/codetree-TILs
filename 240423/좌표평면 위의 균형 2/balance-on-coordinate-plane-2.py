b820bbea680e13e8f977654b6d7328f244d8013e
yunbae <kub938@naver.com>
2024-04-23 17:25:30 +0900

    Title: 좌표평면 위의 균형 2, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240423/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,27 @@
+# 구역 1,2,3,4 = 
+# 1. 좌표값이 x>x축(y값)and y>y축(x값)
+# 2. 좌표값이 x>x축 and y<y축
+# 3. x<x축 and y>y축
+# 4. x<x축 and y<y축
+
+
+
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range(n)]
+max_points = []
+
+for i in range(n):
+    for j in range(n):
+        cnt1,cnt2,cnt3,cnt4 = 0,0,0,0
+        for x,y in arr:
+            if x>i and y>j:
+                cnt1+=1
+            elif x>i and y<j:
+                cnt2+=1
+            elif x<i and y>j:
+                cnt3+=1
+            elif x<i and y<j:
+                cnt4+=1
+        max_points.append(max(cnt1,cnt2,cnt3,cnt4))
+
+print(min(max_points))
