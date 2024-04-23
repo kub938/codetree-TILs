48fdbea7a553f14f9280281a1bc72efa55cf7185
yunbae <kub938@naver.com>
2024-04-24 01:05:05 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240424/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240424/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -4,21 +4,21 @@ part_s = []
 ans = 0
 check = False
 c=0
-one_type = True
 for i in range(2,n):
     part_s = s[c:i]
     if s.count(part_s)==1 and check:
-        if one_type:
-            ans = i+1-c
-        else:
-            ans = i-c
+        if part_s.count(part_s[0])==len(part_s):
+            if s[i+1]!=part_s[-1]:
+                ans=i+1-c
+                break
+            continue
+        ans = i-c
         break
     if s.count(part_s)>1:
         check = True
         continue
     if not check and s.count(part_s)==1:
         c=i-1
-        one_type = False
    
 
 print(ans)
