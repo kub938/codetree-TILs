132525d7876c22d6f3a6d09902b1f7776fa3a257
yunbae <kub938@naver.com>
2024-04-24 20:55:00 +0900

    Title: 가장 많이 나온 쌍, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240424/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 240424/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -1,3 +1,15 @@
 n, m = map(int,input().split())
 
-arr = [list(map(int,input().split())) for i in i if i[::-1] not in arr]
+arr = [list(map(int,input().split())) for _ in  range(m)]
+
+for i in range(m):
+    arr[i]=tuple(sorted(arr[i]))
+
+ans = 0
+arr= list(arr)
+
+for i in range(m):
+    cnt = arr.count(arr[i])
+    ans = max(ans,cnt)
+
+print(ans)
