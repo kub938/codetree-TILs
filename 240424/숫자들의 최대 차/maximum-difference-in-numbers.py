dd219220f6d9412f4d1fab33b5fe04380b16a513
yunbae <kub938@naver.com>
2024-04-24 22:44:30 +0900

    Title: 숫자들의 최대 차, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240424/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240424/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -1,14 +1,12 @@
 n,k = map(int,input().split())
 arr = [int(input()) for _ in range(n)]
-arr.sort()
-ans = 0
 
+ans = 0
 for i in range(n):
     cnt=0
-    for j in range(i,n):
-        if abs(arr[i]-arr[j])<=k:
+    for j in range(n):
+        if i<=arr[j]<=i+k:
             cnt+=1
-
     ans = max(ans,cnt)
 
 print(ans)
