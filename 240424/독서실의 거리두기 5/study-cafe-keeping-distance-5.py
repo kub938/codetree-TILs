98a112962c06526c52ddfab59f75ad4fbb2f3cc8
yunbae <kub938@naver.com>
2024-04-24 16:40:31 +0900

    Title: 독서실의 거리두기 5, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240424/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = list(map(int,input()))
+
+cnt = 0
+check = 0
+max_len = 0
+
+
+for i in range(n):
+    if arr[i]!=0:
+        continue
+    arr[i]=1
+    min_len = 100
+    for j in range(1,n):
+        if arr[j]==1:
+            min_len = min(min_len,cnt+1)
+            cnt=0
+        else:
+            cnt+=1                
+    arr[i]=0
+    max_len = max(max_len, min_len)
+    
+print(min_len)
