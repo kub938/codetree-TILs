fc62777f9a4bacd65a8928fcb63a58ef967cb06f
yunbae <kub938@naver.com>
2024-04-25 01:21:23 +0900

    Title: 이상한 폭탄 3, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240425/이상한 폭탄 3/strange-bomb-3.py
+++ 240425/이상한 폭탄 3/strange-bomb-3.py
@@ -5,13 +5,16 @@ booms = []
 for i in range(n):
     target = arr[i]
     boom = arr[i+1:i+1+k].count(target)
-    booms.append([boom,target])
+    if boom>0:
+        booms.append([boom,target])
 
 ans = 0
 max_value = 0
 for i in range(len(booms)):
-    if booms[i][0]>=max_value:
+    if booms[i][0]>max_value:
         max_value=booms[i][0]
         ans=booms[i][1]
+    if booms[i][0]==max_value:
+        max(ans,booms[i][1])
 
 print(ans)
