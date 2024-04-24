8a4b3abac5d9b3de74ecaac51009e86d04af910d
yunbae <kub938@naver.com>
2024-04-24 20:15:14 +0900

    Title: 가장 작은 x 찾기, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240424/가장 작은 x 찾기/find-smallest-x.py
+++ 240424/가장 작은 x 찾기/find-smallest-x.py
@@ -3,18 +3,20 @@ hint = [list(map(int,input().split())) for _ in range(n)]
 check = False
 ans = 10001
 
-for i in range(hint[0][0],hint[0][1]+1):
+
+
+for i in range(hint[0][1]+1):
     j = 1
     x=i
     for a,b in hint:
         x*=2
-          
         if a<=x<=b:
             check=True
         else:
             check=False
             break
         j+=1
+
     if check:
         ans = min(i,ans)
     
