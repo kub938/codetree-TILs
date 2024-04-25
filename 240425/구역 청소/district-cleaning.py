714f37a57784d24c7ce7256c515065f9cd379787
yunbae <kub938@naver.com>
2024-04-25 20:47:22 +0900

    Title: 구역 청소, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240425/구역 청소/district-cleaning.py
+++ 240425/구역 청소/district-cleaning.py
@@ -3,9 +3,12 @@ c,d = map(int,input().split())
 arr = [0]*101
 
 for i in range(a,b+1):
-    arr[i]=1
+    arr[i]+=1
 
 for i in range(c,d+1):
-    arr[i]=1
+    arr[i]+=1
 
-print(arr.count(1)-1)
+if 2 in arr:
+    print(arr.count(1)+arr.count(2)-1)
+else:
+    print(arr.count(1)-2)
