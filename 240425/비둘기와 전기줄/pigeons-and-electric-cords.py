190227919db0cc52e87ace289723e6dbe1b4db1e
yunbae <kub938@naver.com>
2024-04-25 22:59:54 +0900

    Title: 비둘기와 전기줄, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240425/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 240425/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -6,11 +6,10 @@ cnt=0
 
 
 for p,l in arr:
-    if line[p]==-1:
-        line[p] = l
-    elif line[p]==0 and l==1:
+    if line[p]==0 and l==1:
         cnt+=1
     elif line[p]==1 and l==0:
         cnt+=1
+    line[p] = l
 
 print(cnt)
