ec17c991ac14649b7415fe11d9e206abded2dc0b
yunbae <kub938@naver.com>
2024-04-26 02:15:23 +0900

    Title: 코딩톡, Time: 214ms, Memory: 24MB, Status: Passed - CodeTree

--- 240426/코딩톡/coding-talk.py
+++ 240426/코딩톡/coding-talk.py
@@ -1,40 +1,29 @@
+import sys
+
 n,m,p = map(int,input().split())
-
 arr = [tuple(input().split()) for _ in range(m)]
+target_v = int(arr[p-1][1])
+target_a = arr[p-1][0]
 
-people = [chr(65+i) for i in range(n)]
+if target_v==0:
+    sys.exit()
 
-pos = 0
-# 초기셋팅 : 첫번째 1이상값이 나오면 
-for i in range(m):
-    if arr[i][1]!='0':
-        for j in range(i,m):
-            if arr[j][0] in people:
-                people.pop(people.index(arr[j][0]))
-        pos = i
-        break
-
-tmp = []
-for i in range(pos,p-1):
-    if arr[i][1]!=arr[i-1][1]:  #값이 증가하면 안읽은 사람이 한사람 늘어난 것이므로 이전에 답장했던 사람을 추가
-        if tmp:
-            for e in tmp:
-                people.append(e)
-        else:
-            people.append(arr[i-1][0])
-        people.append(arr[i][0])
-        
-    else:
-        tmp.append(arr[i][0])
-
-for i in range(p,m):
-    for j in range(i,m):
-        if arr[j][0] in people:
-            people.pop(people.index(arr[j][0]))
+for i in range(n):
+    person = chr(65+i)
+    read = False
+    for c,u in arr:
+        u = int(u)
+        if u>=target_v and c==person:
+            read = True
+    if not read:
+        print(person,end=" ")
 
 
-people.sort()
-people = set(people)
 
-if arr[p-1][1]!='0':
-    print(*people)
+## 망한코드
+# 초기 idea = people 배열 셋팅후 target의 아래에 있는것만 빼면 정답이 나옴
+# -> 실패 : 위에서 같은 읽은횟수가 나오면 오답, 맨 위에 0이 있으면 답은 ""이 나옴
+# -> 0이상 값이 나올때 까지 for문을 돌려서 그 부분을 target으로 생각하고 끝까지 나오는 알파벳을 pop하여 초기값 설정
+# -> 그후 다시 맨 위에서부터 값이 증가하면 사람이 늘어난것이므로 해당하는 알파벳 추가, 같은 숫자일 경우 tmp에 넣었다가 값이 올라가면 한꺼번에 추가
+# -> 실패 : 같은 알파벳이 나오면 
+# ->
