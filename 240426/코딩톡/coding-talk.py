fa8e6dfff9455d650c04c76ce4e7a07348612b24
yunbae <kub938@naver.com>
2024-04-26 00:48:36 +0900

    Title: 코딩톡, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240426/코딩톡/coding-talk.py
+++ 240426/코딩톡/coding-talk.py
@@ -15,16 +15,18 @@ for i in range(m):
         break
 
 tmp = []
-for i in range(pos,p):
+for i in range(pos,p-1):
     if arr[i][1]!=arr[i-1][1]:  #값이 증가하면 안읽은 사람이 한사람 늘어난 것이므로 이전에 답장했던 사람을 추가
         if tmp:
             for e in tmp:
                 people.append(e)
         else:
             people.append(arr[i-1][0])
+        people.append(arr[i][0])
+        
     else:
         tmp.append(arr[i][0])
-        
+
 for i in range(p,m):
     for j in range(i,m):
         if arr[j][0] in people:
