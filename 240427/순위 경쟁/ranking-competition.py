d525e3fc4d48a8e29e9cee7a9940dbd0232abd5c
yunbae <kub938@naver.com>
2024-04-27 02:51:54 +0900

    Title: 순위 경쟁, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240427/순위 경쟁/ranking-competition.py
+++ 240427/순위 경쟁/ranking-competition.py
@@ -4,13 +4,16 @@ arr = [list(input().split()) for _ in range(n)]
 a_score = 0
 b_score = 0
 c_score = 0
-g_status = 1 # 1:a==b==c 2:a>b,c 3:b>a,c 4:c>a,b 5:a==b 6:a==b 7:b==c 
+g_status = 1 # 1:a==b==c 2:a>b,c 3:b>a,c 4:c>a,b 5:a==b 6:a==c 7:b==c 
 
 ans=0
+cnt=0
 for c,s in arr:
+    cnt+=1
     s = int(s)
     if s==0:
         continue
+
     if c=='A':
         a_score+=s
     elif c=='B':
@@ -21,23 +24,29 @@ for c,s in arr:
     if a_score==b_score==c_score and g_status!=1:
         ans+=1
         g_status=1
+
     elif a_score>b_score and a_score>c_score and g_status!=2:
         ans+=1
         g_status=2
     elif b_score>a_score and b_score>c_score and g_status!=3:
         ans+=1
         g_status=3
+
     elif c_score>a_score and c_score>b_score and g_status!=4:
         ans+=1
         g_status=4
-    elif a_score==b_score and g_status!=5:
+
+    elif c_score>a_score and a_score==b_score and g_status!=5:
         ans+=1
         g_status=5
-    elif a_score==c_score and g_status!=6:
+
+    elif b_score>a_score and a_score==c_score and g_status!=6:
         ans+=1
         g_status=6
-    elif b_score==c_score and g_status!=7:
+        
+    elif a_score>b_score and b_score==c_score and g_status!=7:
         ans+=1
         g_status=7
+
 
 print(ans)
