1dd291a18c77dab4abeed4d320382bf7150fe5b0
yunbae <kub938@naver.com>
2024-04-26 22:47:39 +0900

    Title: 독서실의 거리두기 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240426/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240426/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -1,57 +1,31 @@
 n = int(input())
 arr = list(map(int,input()))
 
-max_cnt = 0
-cnt = 0
 x1,x2 = 0,0
 max_dis = 0
+ans=0
+dis1= 0
+dis2= 0
 
-last_zero = False
-for i in range(1):
+for i in range(n):
     for j in range(i+1,n):
-        if arr[i]==1 and j==n-1 and arr[-1]==0 and j-i>max_dis:
-            max_dis=j-i
-            last_zero=True
-        elif arr[i]==1 and (arr[j]==1 or j==n-1):
+        if arr[i]==1 and arr[j]==1:
             if max_dis<j-i:
                 max_dis = j-i
                 x1,x2 = i,j
             i=j
-            
 
-if last_zero:
-    arr[-1]=1
+if max_dis%2==0:
+    dis1 = max_dis//2
 else:
-    arr[(x1+x2)//2+x1] = 1
+    dis1 = max_dis//2+1
 
-for i in range(1):
-    if arr[i]==1:
-        for j in range(i+1,n):
-            if arr[j]==1:
-                if max_dis<j-i:
-                    max_dis = j-i
-                    x1,x2 = i,j
-                i=j
-            elif j==n-1 and arr[-1]==0 and j-i>max_dis:
-                print(j-i,j,i)
-                max_dis=j-i
-                last_zero=True
-            
+ans = dis1
 
-if last_zero:
-    arr[-1]=1
-else:
-    arr[(x1+x2)//2+x1] = 1
+if arr[-1]==0:
+    dis2 = n-1-x2
 
+if dis1<dis2:
+    ans = dis2
 
-
-min_dis = 1001
-for i in range(n):
-    for j in range(i+1,n):
-        if arr[i]==1 and arr[j]==1:
-            if min_dis>j-i:
-                min_dis = j-i
-                x1,x2 = i,j
-            i=j
-
-print(min_dis)
+print(ans)
