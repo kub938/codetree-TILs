44b06a9cbedd74addeb20f098a07a46247147224
yunbae <kub938@naver.com>
2024-04-26 17:26:10 +0900

    Title: 연속된 숫자 만들기 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240426/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240426/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,15 +1,22 @@
 a,b,c = map(int,input().split())
 
-cnt = 0
-while b-a>1 or c-b>1:
-    if b-a!=1 and b-a<=c-b :
-        c = a+(b-a)//2
-        b,c = c,b
-        cnt+=1
-    else:
-        a = b+(c-b)//2
-        a,b = b,a
-        cnt+=1
+# cnt = 0
+# while b-a>1 or c-b>1:
+#     if b-a!=1 and b-a<=c-b or c-b==1 :
+#         c = a+(b-a)//2
+#         b,c = c,b
+#         cnt+=1
+#     else:
+#         a = b+(c-b)//2
+#         a,b = b,a
+#         cnt+=1
+#         print(a,b,c)
 
 
-print(cnt)
+# print(cnt)
+
+
+if a+1 == b and a+2==c:
+    print(0)
+else:
+    print(2)
