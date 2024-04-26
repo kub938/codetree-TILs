8cbf8fd7f86ee6414276f378d76bdf6b1f27b9aa
yunbae <kub938@naver.com>
2024-04-26 23:50:58 +0900

    Title: ABC 줄 세우기, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240426/ABC 줄 세우기/abc-line-up.py
+++ 240426/ABC 줄 세우기/abc-line-up.py
@@ -1,23 +1,21 @@
+# 변수 선언 및 입력:
 n = int(input())
-arr = list(input().split())
+arr = [0] + list(input().split())
+ans = 0
 
-sorted_arr = [chr(65+i) for i in range(n)]
+# 'A' 부터 알파벳 순서대로 찾아 가장 앞으로 이동합니다.
+for i in range(1, n + 1):
+    x = chr(ord('A') + i - 1)
 
-
-move = 0
-i = 0
-while arr != sorted_arr:
-    if arr[i]!= sorted_arr[i]:
-        x = arr.index(sorted_arr[i])
-        move += x-i
-        a = arr.pop(x)
-        arr.insert(i,a)
-    i+=1
-        
-
-
-        
+    # i번째 알파벳을 찾아 idx에 저장합니다.
+    idx = 0
+    for j in range(1, n + 1):
+        if arr[j] == x:
+            idx = j
     
+    # idx번째 알파벳을 i번째 위치까지 swap합니다.
+    for j in range(idx - 1, i - 1, -1):
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        ans += 1
 
-
-print(move)
+print(ans)
