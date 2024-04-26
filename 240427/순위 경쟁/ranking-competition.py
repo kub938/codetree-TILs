a324fd4833db141ec4f584d191f02a864e525614
yunbae <kub938@naver.com>
2024-04-27 02:55:59 +0900

    Title: 순위 경쟁, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240427/순위 경쟁/ranking-competition.py
+++ 240427/순위 경쟁/ranking-competition.py
@@ -24,6 +24,7 @@ for c,s in arr:
     if a_score==b_score==c_score and g_status!=1:
         ans+=1
         g_status=1
+        print(c,s,'state1')
 
     elif a_score>b_score and a_score>c_score and g_status!=2:
         ans+=1
@@ -36,15 +37,15 @@ for c,s in arr:
         ans+=1
         g_status=4
 
-    elif c_score>a_score and a_score==b_score and g_status!=5:
+    elif c_score<a_score and a_score==b_score and g_status!=5:
         ans+=1
         g_status=5
 
-    elif b_score>a_score and a_score==c_score and g_status!=6:
+    elif b_score<a_score and a_score==c_score and g_status!=6:
         ans+=1
         g_status=6
         
-    elif a_score>b_score and b_score==c_score and g_status!=7:
+    elif a_score<b_score and b_score==c_score and g_status!=7:
         ans+=1
         g_status=7
 
