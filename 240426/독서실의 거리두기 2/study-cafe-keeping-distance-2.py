55b981435e14fcd29842f8bfef7041c5000c6dc1
yunbae <kub938@naver.com>
2024-04-26 22:25:32 +0900

    Title: 독서실의 거리두기 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240426/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240426/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -7,12 +7,12 @@ x1,x2 = 0,0
 max_dis = 0
 
 last_zero = False
-for i in range(n):
+for i in range(1):
     for j in range(i+1,n):
-        if j==n-1 and arr[-1]==0 and j-i>max_dis:
+        if arr[i]==1 and j==n-1 and arr[-1]==0 and j-i>max_dis:
             max_dis=j-i
             last_zero=True
-        if arr[i]==1 and (arr[j]==1 or j==n-1):
+        elif arr[i]==1 and (arr[j]==1 or j==n-1):
             if max_dis<j-i:
                 max_dis = j-i
                 x1,x2 = i,j
@@ -24,21 +24,25 @@ if last_zero:
 else:
     arr[(x1+x2)//2+x1] = 1
 
-for i in range(n):
-    for j in range(i+1,n):
-        if j==n-1 and arr[-1]==0 and j-i>max_dis:
-            max_dis=j-i
-            last_zero=True
-        if arr[i]==1 and (arr[j]==1 or j==n-1):
-            if max_dis<j-i:
-                max_dis = j-i
-                x1,x2 = i,j
-            i=j
+for i in range(1):
+    if arr[i]==1:
+        for j in range(i+1,n):
+            if arr[j]==1:
+                if max_dis<j-i:
+                    max_dis = j-i
+                    x1,x2 = i,j
+                i=j
+            elif j==n-1 and arr[-1]==0 and j-i>max_dis:
+                print(j-i,j,i)
+                max_dis=j-i
+                last_zero=True
+            
 
 if last_zero:
     arr[-1]=1
 else:
     arr[(x1+x2)//2+x1] = 1
+
 
 
 min_dis = 1001
