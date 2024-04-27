4d8b53178520c0fda5ccfd8e3f907082d601745f
yunbae <kub938@naver.com>
2024-04-28 00:06:04 +0900

    Title: 가장 많은 문제 맞히기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240428/가장 많은 문제 맞히기/answer-the-most-questions.py
@@ -0,0 +1,22 @@
+n,t = map(int,input().split())
+
+arr=[list(map(int,input().split())) for _ in range(n)]
+
+best = [[0,0,0] for _ in range(n)]
+for i in range(n):
+    k,s = arr[i]
+    best[i] = [s/k,i,k] #1시간에 얻을 수 있는 효율
+
+
+best.sort(key =lambda x:(-x[0],x[2]))
+ans = 0
+time = 0
+print(best)
+
+for i in range(n):
+    time += arr[best[i][1]][0]
+    if time>=t:
+        break
+    ans += arr[best[i][1]][1]
+
+print(ans)
