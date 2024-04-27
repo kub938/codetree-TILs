033fa6330e918e8f743b52fab695e71bee8d5a29
yunbae <kub938@naver.com>
2024-04-27 23:28:28 +0900

    Title: 수열의 순서 바꾸기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240427/수열의 순서 바꾸기/reorder-sequence.py
+++ 240427/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,36 +1,12 @@
 n = int(input())
 arr = list(map(int,input().split()))
-sorted_arr = sorted(arr)
 
-# 젤 작은값은 젤 큰값 뒤로
-# 젤 큰값은 del 하고 sorted_arr.pop()
-# 중간 값은 젤 작은값 뒤로
 
-ans = 0
+i = n-2
+
 while 1:
-    if sorted_arr == arr:
+    if arr[i-1]>arr[i]:
         break
-    while arr[-1]==sorted_arr[-1]:
-        arr.pop()
-        sorted_arr.pop()
-    max_n = max(arr)
-    min_n = min(arr)
-    if arr[0]==max_n:
-        del arr[0]
-        sorted_arr.pop()
-    elif arr[0]==min_n:
-        if arr[-1]==sorted_arr[1]:
-            arr.insert(n-1,arr[0])
-        else:
-            max_idx = arr.index(max_n)
-            arr.insert(max_idx+1,arr[0])
-        del arr[0]
-    else:
-        min_idx = arr.index(min_n)
-        arr.insert(min_idx+1,arr[0])
-        del arr[0]
+    i-=1
 
-    ans+=1
-
-
-print(ans)
+print(n-(n-i))
