e39e574162a1fbe5b77d3f24962efa52e1f61749
yunbae <kub938@naver.com>
2024-04-27 17:50:01 +0900

    Title: 홀수 짝수의 묶음, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240427/홀수 짝수의 묶음/odd-even-bundle.py
@@ -0,0 +1,24 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+even, odd = 0,0
+ans = 0
+
+for i in arr:
+    if i%2 == 0:
+        even+=1
+    else:
+        odd+=1
+
+if odd==even:
+    ans = even 
+elif odd<even:
+    ans = even+1
+else:
+    ans+=even*2
+    if (odd-even)%3==2:
+        ans+=1
+    elif (odd-even)%3==1:
+        ans-=1
+
+print(ans)
