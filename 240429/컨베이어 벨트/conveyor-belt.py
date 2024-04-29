bfb587a98220e43099eac269b56199ea723cd1c3
yunbae <kub938@naver.com>
2024-04-29 23:11:42 +0900

    Title: 컨베이어 벨트, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240429/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,14 @@
+n,t = map(int,input().split())
+
+arr1 = list(map(int,input().split()))
+arr2 = list(map(int,input().split()))
+
+for i in range(t):
+    tmp1,tmp2 = arr1[-1],arr2[-1]
+    arr1 = [0]+arr1[:n-1]
+    arr2 = [0]+arr2[:n-1]
+    arr1[0] = tmp2
+    arr2[0] = tmp1
+
+print(*arr1)
+print(*arr2)
