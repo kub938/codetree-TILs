6786df3e1bedbed928f093a5cdb13b63d5affd6e
yunbae <kub938@naver.com>
2024-05-08 18:16:33 +0900

    Title: 갈 수 있는 곳들, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240508/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+n,K = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(n)]
+k=[list(map(int,input().split())) for _ in range(K)]
+dis = [[0]*n for _ in range(n)]
+
+que = deque()
+dx,dy = [0,1,0,-1],[1,0,-1,0]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def bfs(x,y):
+    que.append([x,y])
+    while que:
+        x,y = que.popleft()
+        for i in range(4):
+            nx,ny = x+dx[i],y+dy[i]
+            if in_range(nx,ny) and board[nx][ny]==0 and dis[nx][ny]==0:
+                que.append([nx,ny])
+                dis[nx][ny]=1
+
+for x,y in k:
+    bfs(x,y)
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        if dis[i][j]==1:
+            ans+=1
+
+print(ans)
