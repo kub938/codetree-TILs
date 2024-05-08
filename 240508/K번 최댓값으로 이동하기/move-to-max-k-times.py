b3b5333e693dd001c25f6a81a72f671a4a378962
yunbae <kub938@naver.com>
2024-05-08 21:07:46 +0900

    Title: K번 최댓값으로 이동하기, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240508/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+n,k = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(n)]
+x,y = map(int,input().split())
+x,y = x-1,y-1
+que = deque()
+cnt = 0
+dx,dy = [0,1,0,-1],[1,0,-1,0]
+
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def bfs(x,y,cnt):
+    if cnt==k:
+        return x+1,y+1
+    que.append([x,y])
+    cnt+=1
+    max_value = 0
+    value = [[0]*n for _ in range(n)]
+    max_num = board[x][y]
+    while que:
+        x,y = que.popleft()
+        for i in range(4):
+            nx,ny = x+dx[i], y+dy[i]
+            if in_range(nx,ny) and board[nx][ny]<max_num and value[nx][ny]==0:
+                que.append([nx,ny])
+                value[nx][ny] = board[nx][ny]
+                max_value = max(max_value,value[nx][ny])
+    
+    for i in range(n):
+        for j in range(n):
+            if value[i][j]==max_value:
+                x,y = i,j
+                return bfs(x,y,cnt)
+                    
+
+x,y = bfs(x,y,cnt)
+
+print(x,y)
