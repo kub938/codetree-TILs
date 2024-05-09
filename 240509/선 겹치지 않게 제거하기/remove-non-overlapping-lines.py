03434fbc1b7bfcd47536b1a7033c0281aa3e7515
yunbae <kub938@naver.com>
2024-05-09 15:53:08 +0900

    Title: 선 겹치지 않게 제거하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/선 겹치지 않게 제거하기/remove-non-overlapping-lines.py
+++ 240509/선 겹치지 않게 제거하기/remove-non-overlapping-lines.py
@@ -5,11 +5,14 @@ for i in range(n):
     a,b = map(int,input().split())
     if a>b:
         a,b = b,a
-    if a==b:
+        for j in range(a,b+1):
+            line[j]+=1
+    elif a==b:
         point.append(a)
     else:
-        for j in range(a,b):
+        for j in range(a,b+1):
             line[j]+=1
+
 if point:
     for i in point:
         if line[i-1]==0 or line[i+1]==0:
