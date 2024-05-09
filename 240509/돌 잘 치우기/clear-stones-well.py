97a25fed38d9089300ae6a37ced2fadd42e3bff9
yunbae <kub938@naver.com>
2024-05-09 21:03:12 +0900

    Title: 돌 잘 치우기, Time: 228ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240509/돌 잘 치우기/clear-stones-well.py
@@ -0,0 +1,51 @@
+from collections import deque
+from itertools import combinations
+import copy
+
+n,k,m = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(n)]
+
+dx,dy = [0,1,0,-1],[1,0,-1,0]
+cnt=0
+que = deque()
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def bfs(board,x,y):
+    cnt=0
+    dis = [[0] * n for _ in range(n)]
+    que.append([x,y])
+    while que:
+        x,y = que.popleft()
+        for i in range(4):
+            nx,ny = x+dx[i],y+dy[i]
+            if in_range(nx,ny) and dis[nx][ny]==0 and board[nx][ny]==0:
+                que.append([nx,ny])
+                dis[nx][ny]= 1
+    for i in range(n):
+        for j in range(n):
+            if dis[i][j]==1:
+                cnt+=1
+    return cnt
+
+stone_pos = []
+for i in range(n):
+    for j in range(n):
+        if board[i][j]==1:
+            stone_pos.append([i,j])
+
+max_value = 0
+
+start_point = [list(map(int,input().split())) for _ in range(k)]
+for comb in combinations(stone_pos,m):
+    copy_board = copy.deepcopy(board)
+    for i in comb:
+        x,y = i
+        copy_board[x][y]=0
+        for j in start_point:
+            r,c = j
+            r,c = r-1,c-1
+            max_value = max(max_value,bfs(copy_board,r,c))
+
+print(max_value)
