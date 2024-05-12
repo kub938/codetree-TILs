d96b0a812a1b7697920df3b61cc176dbdb8a50b1
yunbae <kub938@naver.com>
2024-05-12 19:56:46 +0900

    Title: 사각형 채우기 3, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240512/사각형 채우기 3/rectangle-fill-3.py
+++ 240512/사각형 채우기 3/rectangle-fill-3.py
@@ -2,6 +2,6 @@ n = int(input())
 arr = [2,7,22]
 
 for i in range(3,n):
-    arr.append(arr[i-1]*3+arr[i-2]-arr[i-3]%1000000007)
+    arr.append((arr[i-1]*3+arr[i-2]-arr[i-3])%1000000007)
 
 print(arr[n-1])
