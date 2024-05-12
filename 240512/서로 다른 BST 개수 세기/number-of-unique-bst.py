5829708b03d1ab210a18420a59785ce65fd8b215
yunbae <kub938@naver.com>
2024-05-12 20:56:21 +0900

    Title: 서로 다른 BST 개수 세기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240512/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 240512/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -1,7 +1,8 @@
-dp = [1,2,5]
+dp = [1,2]
 n = int(input())
 
-for i in range(4,n):
-    dp.append(dp[i-3]+dp[i-2]+dp[i-1])
+
+for i in range(2,n):
+    dp.append(dp[i-1]*dp[i-2]+i+1)
 
 print(dp[n-1])
