6749c0f476e9f6c9a530cab937fe3fee6b6a8ed6
yunbae <kub938@naver.com>
2024-05-14 21:08:44 +0900

    Title: 정수 사각형 최대 합, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240514/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240514/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -6,10 +6,7 @@ sum_line[0][0] = board[0][0]
 for i in range(1,n):
     sum_line[0][i]=sum_line[0][i-1]+board[0][i]
     sum_line[i][0]=sum_line[i-1][0]+board[i][0]
-for i in range(1,n):
-    sum_line[n-1][i]=sum_line[n-1][i-1]+board[n-1][i]
-    sum_line[i][n-1]=sum_line[i-1][n-1]+board[i][n-1]
-    sum_line[n-1][n-1]=max(sum_line[n-1][n-2]+board[n-1][n-1],sum_line[n-2][n-2]+board[n-1][n-1])
+
 
 for i in range(1,n):
     for j in range(1,n):
