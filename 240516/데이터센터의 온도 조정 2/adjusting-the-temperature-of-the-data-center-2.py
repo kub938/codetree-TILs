d99b736acf8ecdedf2c7b82e453d5e2502263f91
yunbae <kub938@naver.com>
2024-05-16 01:02:12 +0900

    Title: 데이터센터의 온도 조정 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240516/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,21 @@
+n,c,g,h = map(int,input().split())
+
+t = [list(map(int,input().split())) for _ in range(n)] 
+arr = [0]*1000
+max_idx = 0
+for ta,tb in t:
+    for i in range(ta,tb+1):
+        arr[i]+=1
+
+target = arr.index(max(arr))
+
+ans = 0
+for ta,tb in t:
+    if ta<=target<=tb:
+        ans+=g
+    elif target<ta:
+        ans+=c
+    elif target>tb:
+        ans+=h
+
+print(ans)
