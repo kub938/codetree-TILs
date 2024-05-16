50fdc16768b427b2bbcd8f21316292847a9d96b4
yunbae <kub938@naver.com>
2024-05-16 17:00:06 +0900

    Title: 흥미로운 숫자 2, Time: 282ms, Memory: 25MB, Status: Passed - CodeTree

--- 240516/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240516/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,11 +1,12 @@
-from collections import Counter
 x,y = map(int,input().split())
 
 ans = 0
 for i in range(x,y+1):
-    i = str(i)
-    count_n = Counter(i)
-    if len(count_n)==2:
+    i = list(str(i))
+    i = sorted(list(map(int,i)))
+    target1 = i[0]
+    target2 = i[-1]
+    if len(i)-i.count(target1)==1 or len(i)-i.count(target2)==1:
         ans+=1
-
+        
 print(ans)
