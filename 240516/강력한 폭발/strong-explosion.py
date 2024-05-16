d54cd6003d4fe591e4ad4621d98cd5dee7279123
yunbae <kub938@naver.com>
2024-05-16 20:52:40 +0900

    Title: 강력한 폭발, Time: 680ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240516/강력한 폭발/strong-explosion.py
+++ 240516/강력한 폭발/strong-explosion.py
@@ -1,34 +1,37 @@
 def solved(m):
+    global ans
+
     if m==0:
         ans = max(boom(bombs),ans)
         return
     
-    for i in target:
+    for i in range(3):
         bombs.append(i)
         solved(m-1)
         bombs.pop()
 
 def in_range(x,y):
-    return 0<=x<=n and 0<=y<=n
+    return 0<=x<n and 0<=y<n
 
+def boom(bombs):
+    bomb_shapes = [
+          [[2,0],[1,0],[0,0],[-1,0],[-2,0]],
+        [[1,0],[0,1],[0,0],[-1,0],[-1,-1]],
+          [[-1,-1],[-1,1],[0,0],[1,1],[1,-1]]
+    ]
 
-bomb_shapes = 
-[[[2,0],[1,0],[0,0],[-1,0],[-2,0]],
-[[1,0],[0,1],[0,0],[-1,0],[-1,-1]],
-[[-1,-1],[-1,1],[0,0],[1,1],[1,-1]]]
-
-
-def boom(coords):
-    dis = [[0]*n for _ in range(n)]
-    cnt = 0
-    for x,y in coords:
-        for i in range(3):
+    max_cnt = 0
+    for i in bombs:
+        dis = [[0]*n for _ in range(n)]
+        cnt=0
+        for x,y in target:
             for j in range(5):
-                nx,ny = x+bomb_shapes[i][j][0], y+bomb_shapes[i][j][1]
+                nx,ny = x+bomb_shapes[i][j][0],y+bomb_shapes[i][j][1]
                 if in_range(nx,ny) and dis[nx][ny]==0:
                     dis[nx][ny]=1
                     cnt+=1
-    return cnt
+        max_cnt = max(max_cnt,cnt)
+    return max_cnt
         
 
 n = int(input())
