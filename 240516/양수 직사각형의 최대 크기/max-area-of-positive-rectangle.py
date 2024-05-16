afb144aa52a1d1cfe835d8863cecbcb4444084aa
yunbae <kub938@naver.com>
2024-05-16 23:31:23 +0900

    Title: 양수 직사각형의 최대 크기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240516/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240516/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -3,13 +3,13 @@ board = [list(map(int,input().split())) for _ in range(n)]
 
 def col_search(x,y):
     for c in range(y,m):
-        if board[x][c]<0:
+        if board[x][c]<=0:
             return c-1    #음수 만나면 그 col값 반환
     return c
 
 def row_search(x,y):
     for r in range(x,n):
-        if board[r][y]<0:
+        if board[r][y]<=0:
             return r-1
     return r
 
@@ -22,10 +22,10 @@ for x in range(n):
             for i in range(x,nx+1):
                 cnt=0
                 for j in range(y,ny+1):
-                    if board[i][j]<0:
+                    if board[i][j]<=0:
                         break
                     cnt+=1
-                if board[i][j]<0:    
+                if board[i][j]<=0:    
                     break
                 else:
                     result+=cnt
