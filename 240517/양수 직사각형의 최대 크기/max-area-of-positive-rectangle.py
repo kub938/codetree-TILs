648df901ecfdc2b4236702ffbe7ecb88e8d07bad
yunbae <kub938@naver.com>
2024-05-17 23:58:36 +0900

    Title: 양수 직사각형의 최대 크기, Time: 418ms, Memory: 29MB, Status: Passed - CodeTree

--- 240517/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240517/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,16 +1,16 @@
 n,m = map(int,input().split())
 board = [list(map(int,input().split())) for _ in range(n)]
 
-def rect_size(x1, y1, x2, y2):
-    size = 0
-    if check(x1, y1, x2, y2): # 음수가 없다면
-        rect = [
-            board[i][j]
-            for i in range(x1, x2+1)
-            for j in range(y1, y2+1)
-        ]
-        size = len(rect)
-    return size
+# def rect_size(x1, y1, x2, y2):
+#     size = 0
+#     if check(x1, y1, x2, y2): # 음수가 없다면
+#         rect = [
+#             board[i][j]
+#             for i in range(x1, x2+1)
+#             for j in range(y1, y2+1)
+#         ]
+#         size = len(rect)
+#     return size
 
 def check(x1,y1,x2,y2):
     size = 0
@@ -18,11 +18,13 @@ def check(x1,y1,x2,y2):
         x1,x2=x2,x1
     elif y1>y2:
         y1,y2 = y2,y1
+    cnt=0
     for x in range(x1,x2+1):
         for y in range(y1,y2+1):
             if board[x][y]<=0:
                 return False
-    return True
+            cnt+=1
+    return cnt
 
 ans = -1
 s = 0
@@ -30,7 +32,7 @@ for i in range(n):
     for j in range(m):
         for k in range(n):
             for l in range(m):
-                s = rect_size(i,j,k,l)
+                s = check(i,j,k,l)
                 if s:
                     ans = max(s,ans)
 
