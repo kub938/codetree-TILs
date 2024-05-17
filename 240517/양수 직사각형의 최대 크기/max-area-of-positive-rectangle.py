8c5942bad823c27216fb3086add15f9e86b546c3
yunbae <kub938@naver.com>
2024-05-17 23:53:30 +0900

    Title: 양수 직사각형의 최대 크기, Time: 165ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240517/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240517/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -12,7 +12,6 @@ def check(x1,y1,x2,y2):
             for y in range(y1,y2+1):
                 if board[x][y]<=0:
                     return False
-        print(abs(x2-x1+1)*abs(y2-y1+1),x1,y1,x2,y2)
         return abs(x2-x1+1)*abs(y2-y1+1)
     return False
 
