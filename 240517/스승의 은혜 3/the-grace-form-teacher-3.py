187052521dd207432e01c16bc9dc47cf6dd2ff95
yunbae <kub938@naver.com>
2024-05-17 21:29:43 +0900

    Title: 스승의 은혜 3, Time: 677ms, Memory: 27MB, Status: Passed - CodeTree

--- 240517/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240517/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -11,11 +11,13 @@ for i in range(n):
     p_list = []
     for j in range(n):
         p_list.append(sale[j][0]+sale[j][1])
+    p_list.sort()
+
     cnt = 0
     sum_p =0
     for j in range(n):
         sum_p+=p_list[j]
-        if sum_p>=b:
+        if sum_p>b:
             break
         cnt+=1
     ans = max(cnt,ans)
