2a66d55ad8ed8ea2c0f4b9ce885d48dd4b4c199e
yunbae <kub938@naver.com>
2024-05-18 00:24:56 +0900

    Title: 그 요일은, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240518/그 요일은/the-day-of-the-day.py
+++ 240518/그 요일은/the-day-of-the-day.py
@@ -3,11 +3,15 @@ day = input()
 
 month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
 days = {'Mon':0,'Tue':1,'Wed':2,'Thu':3,'Fri':4,'Sat':5,'Sun':6}
+
 sum_month = sum(month[m1+1:m2-1])
-cal_day = sum_month+month[m1]-d1+d2
+
+if m1==m2:
+    cal_day = d2-d1
+else:
+    cal_day = sum_month+month[m1]-d1+d2
+
 ans = cal_day//7
-
-
 if days[day]<=cal_day%7:
     ans+=1
 
