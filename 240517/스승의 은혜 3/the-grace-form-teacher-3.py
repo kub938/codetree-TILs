4220bf82231b8a6397fd191c95fac55297e3ecae
yunbae <kub938@naver.com>
2024-05-17 21:24:05 +0900

    Title: 스승의 은혜 3, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240517/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240517/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -8,7 +8,6 @@ ans = 0
 for i in range(n):
     sale = copy.deepcopy(price)
     sale[i][0]//=2
-    print(sale)
     p_list = []
     for j in range(n):
         p_list.append(sale[j][0]+sale[j][1])
