1fd0a13875484bf053e4d872c81ba368d993cefb
yunbae <kub938@naver.com>
2024-05-18 22:30:48 +0900

    Title: 숫자 합치기, Time: 5035ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240518/숫자 합치기/merge-numbers.py
+++ 240518/숫자 합치기/merge-numbers.py
@@ -2,10 +2,11 @@ n = int(input())
 arr = list(map(int,input().split()))
 
 price = 0
-while len(arr)!=1:
+while n!=1:
     arr.sort()
     arr[0]=arr[0]+arr[1]
     price += arr[0]
     del arr[1]
+    n-=1
 
 print(price)
