90a7c96c7fe2021ca2711b5a95045861c92f0c17
yunbae <kub938@naver.com>
2024-05-18 18:55:56 +0900

    Title: 줄 세우기 2, Time: 229ms, Memory: 25MB, Status: Passed - CodeTree

--- 240518/줄 세우기 2/line-up-students-2.py
+++ 240518/줄 세우기 2/line-up-students-2.py
@@ -1,7 +1,11 @@
 n = int(input())
-students = [list(map(int,input().split())) for _ in range(n)]
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+students = []
+for i in range(n):
+    students.append([*arr[i],i+1])
 
 students.sort(key=lambda x: (x[0],-x[1]))
 
 for i in range(n):
-    print(*students[i],i+1)
+    print(*students[i])
