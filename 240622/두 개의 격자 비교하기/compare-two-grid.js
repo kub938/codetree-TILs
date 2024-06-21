3a910696d503e3d3c21716eafd35efb98cc21a62
yunbae <kub938@naver.com>
2024-06-22 01:03:38 +0900

    Title: 두 개의 격자 비교하기, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

--- 240622/두 개의 격자 비교하기/compare-two-grid.js
+++ 240622/두 개의 격자 비교하기/compare-two-grid.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split('\n');
-let n = Number(input[0][0])
-let m = Number(input[0][2])
+let n = Number(input[0].split(" ")[0])
+let m = Number(input[0].split(" ")[1])
 
 let arr1 = input.slice(1,n+1).map((num) => num.split(' ').map(Number));
 let arr2 = input.slice(n+1,n*2+1).map((row) => row.split(' ').map(Number));
+
 
 for(let i=0;i<n;i++){
     ans = Array()
