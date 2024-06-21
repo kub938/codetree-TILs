5ca027604f165bf278c56e62c519acbf053fbdb6
yunbae <kub938@naver.com>
2024-06-21 22:42:02 +0900

    Title: 숫자 직사각형, Time: 226ms, Memory: 15MB, Status: Passed - CodeTree

--- 240621/숫자 직사각형/rectangle-of-numbers.js
+++ 240621/숫자 직사각형/rectangle-of-numbers.js
@@ -5,10 +5,8 @@ const n = Number(input[0]);
 const m = Number(input[1]);
 
 arr = Array(n).fill(0).map(()=>Array(m).fill(0))
-console.log(arr)
 
 var cnt = 0
-
 for(let i=0;i<n;i++){
     let ans = ''
     for(let j=0;j<m;j++){
