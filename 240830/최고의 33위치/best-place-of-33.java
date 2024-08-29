46fe1ec8dc82495dfb150d1b2774caeef7683da2
yunbae <kub938@naver.com>
2024-08-30 04:09:47 +0900

    Title: 최고의 33위치, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240830/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int [][] arr = new int[n][n];
+        
+        for(int i=0;i<n;i++){
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+                for(int j=0;j<n;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int maxValue = 0;
+        for(int i=0;i<n-2;i++){
+            for(int j=0;j<n-2;j++){
+                int value = 0;
+                for(int k=i;k<i+3 && i+3<=n;k++){
+                    for(int l=j;l<i+3 && i+3<=n;l++){
+                        value += arr[k][l];
+                    }
+                }
+                maxValue = Math.max(maxValue, value);
+            }
+        }
+        System.out.println(maxValue);
+    }
+}
