3761b35082489f6532c5d93eb4b78e49ec6aa5dc
yunbae <kub938@naver.com>
2024-08-30 03:00:46 +0900

    Title: 점수 입출력, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240830/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println("Your score is "+a+" point.");
+
+    }
+}
