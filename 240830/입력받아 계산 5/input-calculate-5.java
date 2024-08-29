495dfe7c6e5ef996447f079bb030c8bb5a23d3d2
yunbae <kub938@naver.com>
2024-08-30 03:36:40 +0900

    Title: 입력받아 계산 5, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240830/입력받아 계산 5/input-calculate-5.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(input," ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a+b);
+
+    }
+}
