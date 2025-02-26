6ad19b10d8f27380bf455091b1e7fb95c54f0564
yunbae <kub938@naver.com>
2025-02-26 15:44:01 +0900

    Title: 행복한 수열의 개수, Time: 86ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250226/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n,m,result,cnt;
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j]= Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            cnt = 1;
+            for (int j = 1; j < n; j++) {
+                if(arr[i][j] == arr[i][j-1]){
+                    cnt+=1;
+
+                }
+                if(cnt==m){
+                    result+=1;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            cnt = 1;
+            for (int j = 1; j < n; j++) {
+                if(arr[j][i] == arr[j-1][i]){
+                    cnt+=1;
+                }
+                if(cnt==m){
+                    result+=1;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(result);
+
+
+
+
+    }
+}
