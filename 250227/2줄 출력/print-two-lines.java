0a1f8dd3560b1f49b315cf5777f5d60e5bddae9a
yunbae <kub938@naver.com>
2025-02-27 14:36:32 +0900

    Title: 2줄 출력, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

--- 250227/2줄 출력/print-two-lines.java
+++ 250227/2줄 출력/print-two-lines.java
@@ -1,5 +1,8 @@
-public static void main(String args[]) {
-    System.out.println("Hello");
-    System.out.println("World");
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        System.out.println("World");
+    }
 }
+
 
